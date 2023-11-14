11dcfccdba2fb884dbcdff32d0747023ad32862d
Iman <iman.osama2000@gmail.com>
2023-11-14 10:24:48 +0200

    [ref]: shared files

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LandingSubjectCardComponent } from './landing-subject-card/landing-subject-card.component';
-import { SubjectInfoComponent } from './subject-info/subject-info.component';
-import { SelectComponent } from './select/select.component';
+import { LandingSubjectCardComponent } from './cards/landing-subject-card/landing-subject-card.component';
+import { SubjectInfoComponent } from './cards/subject-info/subject-info.component';
+import { SelectComponent } from './input/select/select.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AvatarImgComponent } from './avatar-img/avatar-img.component';
-import { RatingStarsComponent } from './rating-stars/rating-stars.component';
+import { AvatarImgComponent } from './images/avatar-img/avatar-img.component';
+import { RatingStarsComponent } from './other/rating-stars/rating-stars.component';
 
 @NgModule({
   declarations: [
